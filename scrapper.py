1ca9232b454aca2a58c52b764c2e31fd133d00ef
Reshma <reshmasadhu@rapidinnovation.dev>
2023-05-25 12:40:17 +0530

    changes-done
    Some unwanted comments removed

--- scrapper.py
+++ scrapper.py
@@ -4,19 +4,14 @@ import spacy
 import csv
 from datetime import datetime, timedelta
 
-# Function to scrape news articles from a given website
 def scrape_news(url):
-    # Fetch the website content
     response = requests.get(url)
     content = response.content
     
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(content, 'html.parser')
     
-    # Find the news articles
     articles = soup.find_all('article')
     
-    # Extract the relevant information from each article
     news_data = []
     for article in articles:
         title = article.find('h3').text.strip()
@@ -25,21 +20,17 @@ def scrape_news(url):
     
     return news_data
 
-# Function to perform Named Entity Recognition on the news data
 def perform_ner(news_data):
-    # Load the NER model (Spacy's pre-trained model)
     nlp = spacy.load('en_core_web_sm')
     
     for article in news_data:
         title_entities = []
         summary_entities = []
         
-        # Perform NER on the title
         title_doc = nlp(article['Title'])
         for ent in title_doc.ents:
             title_entities.append((ent.text, ent.label_))
         
-        # Perform NER on the summary
         summary_doc = nlp(article['Summary'])
         for ent in summary_doc.ents:
             summary_entities.append((ent.text, ent.label_))
@@ -49,7 +40,6 @@ def perform_ner(news_data):
     
     return news_data
 
-# Function to tag company names from a list of predefined companies
 def tag_companies(news_data, companies):
     for article in news_data:
         tagged_companies = []
@@ -60,7 +50,6 @@ def tag_companies(news_data, companies):
     
     return news_data
 
-# Function to save the news data to a CSV file
 def save_to_csv(news_data, filename):
     keys = ['Title', 'Summary', 'Title Entities', 'Summary Entities', 'Tagged Companies']
     
@@ -73,23 +62,17 @@ def save_to_csv(news_data, filename):
 #url = 'https://www.reuters.com/' 
 url = 'https://www.prnewswire.com'
 
-# Scrape the news articles
 today = datetime.now().date()
 yesterday = today - timedelta(days=1)
 news_data = scrape_news(url)
 
-# Filter the news articles for the last two days
 filtered_news = [article for article in news_data if today >= article['Date'] >= yesterday]
 
-# Perform NER on the news articles
 news_data_with_ner = perform_ner(filtered_news)
 
-# Define a list of predefined companies
 companies = ['IBM Systems', 'Google', 'Meta', 'MarketsandMarkets', 'wipro']
 
-# Tag the company names in the news articles
 news_data_with_tags = tag_companies(news_data_with_ner, companies)
 
-# Save the news data to a CSV file
 filename = 'news_data.csv'
 save_to_csv(news_data_with_tags, filename)
